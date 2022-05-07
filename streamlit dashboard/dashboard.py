329684674981f66a6ea81fb5a957c9a31d6adaf7
Dillon3535 <60332929+Dillon3535@users.noreply.github.com>
2022-05-07 19:33:51 -0400

    Add files via upload

--- streamlit dashboard/dashboard.py
+++ streamlit dashboard/dashboard.py
@@ -1,9 +1,12 @@
 from datetime import date
+from turtle import color
 import altair as alt
+from pyparsing import White
 from sqlalchemy import false
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import streamlit.components.v1 as components 
 
 st.set_page_config(layout = "wide")
 df = pd.read_csv("C:/Users/dillo/OneDrive/Desktop/allFlows_reduced_withdate.csv", low_memory=False)
@@ -11,14 +14,36 @@ df2 = pd.read_csv("C:/Users/dillo/OneDrive/Desktop/Distributionofattacks.csv", l
 
 st.sidebar.title("Real-Time Network Flow DDOS Attacks")
 st.sidebar.header("Dillon Alterio DDOS Dashboard")
-st.sidebar.write("Welcome to my interactive dashboard, this dashboard contains a dataframe that consist of DDOS attacks on a real-time universities network.  Every column in the dataframe represents a network flow.  Please choose the selector box to view various charts and graphs pertaining to the dataset below:")
-page = st.sidebar.selectbox('Please Select a Page:',
-  ['Welcome','All Network Flows', 'Network Flow Attack Timeline','Distribution of Attacks', 'Scatter Plot of Packet Sizes to Attacks','Box plot for Packet Attack Size'])
+st.sidebar.write("""Welcome to my interactive dashboard, this dashboard contains a dataframe that consist of DDOS attacks on a real-time universities network. 
+                    Every column in the dataframe represents a network flow.  Please choose the selector box to view various charts and graphs pertaining to the dataset below:""")
+
+st.header("Real-Time Network Flow DDOS Attacks")
+page = st.sidebar.selectbox('Please Select a Page To View:',
+  ['Welcome','All Network Flows', 'Network Flow Attack Timeline','Distribution of Attacks', 'Scatter Plot of Packet Sizes to Attacks','Line for Packet Attack Size'])
 
 if page == 'Welcome':
-  st.title=("CSC 400 Interactive Dashboard")
-  st.header=("Please Interact with the sidebar to the left in to interact with the dashboard")
-  st.write=("Hello ")
+  components.html("""
+                  <style>
+                    html {background-color: white;}
+                    html {font-family:courier;}
+                    h1 {font-size:300%;}
+                    p {font-size:150%;}
+                  </style>
+
+                  <html>
+                    <body>
+                      <h1>Hello,welcome to my interactive Dashboard</h1>
+                        <p> Attacks on networks are becoming more sophisticated and pose a serious threat to various types of infrastructures. 
+                          Unavailability of services due to various forms of DDOS attacks drastically reduces the confidence in the security of that stored data and the ability for a 
+                          business/infrastructure to operate efficently.  Throughout this dashboard, the user will be able to view various interactive dashboards that pertain to a real-life 
+                          dataset that contains various forms of DDOS attacks.  This dataset includes a ten month timeframe in which network flows were documentated and processed through 
+                          the companies custom built IDS(intrusion detection system).  After their IDS processed the network flows, their system indicates what network flows were DDOS 
+                          attacks and what network flows were not.  With this information, I was able to create various interactive dashboards that the user will be able to access to gain a better 
+                          knowledge on various aspects of DDOS attacks to a real-life network environment.  The user will gain some knowledge in understanding how, when, and through what protocol
+                          these malicious acts were conducted in.</p>
+                    </body>
+                  </html>""", width=1200, height=600,)
+  
 
 if page == 'All Network Flows':
  
@@ -84,7 +109,7 @@ if page == 'Distribution of Attacks':
 if page == 'Scatter Plot of Packet Sizes to Attacks':
   
   clist = df['ID'].unique()
-  col1, col2 , col3= st.columns(3)
+  col1, col2 = st.columns(2)
   
   fig = px.scatter(df, x="ipkt", y="attack_t", color="attack_a", title="This chart shows the packet amounts that were used to attack the network")
   col1.plotly_chart(fig,user_container_width = True)
@@ -95,7 +120,16 @@ if page == 'Scatter Plot of Packet Sizes to Attacks':
 if page == 'Line for Packet Attack Size':
   
   clist = df['ID'].unique()
-  col1, col2 = st.columns(2)
+  col1, col2, col3, col4 = st.columns(4)
   
-  fig = px.line(df,values='attack_a', names= "attack_t", title = "This line graph sho")
-  col2.plotly_chart(fig,use_container_width = True)
+  fig = px.line(df, x='Date', y= ['attack_a','ID'] )
+  col1.plotly_chart(fig)
+
+  fig = px.line(df, x='Monthly', y= ['attack_a','ID'] )
+  col2.plotly_chart(fig)
+
+  fig = px.line(df, x='Weekly', y= ['attack_a','ID'] )
+  col3.plotly_chart(fig)
+
+  fig = px.line(df, x='Daily', y= ['attack_a','ID'] )
+  col4.plotly_chart(fig)
